71412fc959eebcbfa5e14d5da08465475b119042
dcanuto <danny.canuto@gmail.com>
2018-01-24 15:29:35 -0800

    Add relative path expansion in loading artery text file

--- src/loadtexttree.jl
+++ src/loadtexttree.jl
@@ -1,4 +1,4 @@
 function loadtexttree(filename::String)
-    branches = CSV.read(joinpath(Pkg.dir("CVModule"),"src\\",filename),null="");
+    branches = CSV.read(joinpath(abspath(dirname(@__FILE__)),filename),null="");
     return branches
 end
