e4ce9faea499e4b5edcbf435b18e9b9543c04060
dcanuto <danny.canuto@gmail.com>
2018-06-04 11:09:29 -0700

    Fused vector operations in finite differencing for speedup

--- src/vascularmain.jl
+++ src/vascularmain.jl
@@ -1,15 +1,34 @@
 importall CVModule
 
+type CVTimer
+    tfd::Float64
+    tc::Float64
+    ts::Float64
+    tr::Float64
+    tt::Float64
+    function CVTimer()
+        this = new();
+        tfd = 0;
+        tc = 0;
+        ts = 0;
+        tr = 0;
+        tt = 0;
+        return this
+    end
+end
+
 function main()
 
-# filename = "arterytree.csv";
-filename = "hemo2.mat";
+# filename = "arterytree.csv";j
+filename = "test2.mat";
 rstflag = "yes"
-hemoflag = "yes"
+hemoflag = "no"
 saveflag = "yes"
 coupleflag = "no"
 
-system = CVModule.buildall(filename;numbeatstotal=10,restart=rstflag,injury=hemoflag);
+system = CVModule.buildall(filename;numbeatstotal=1,restart=rstflag,injury=hemoflag);
+
+times = CVTimer();
 
 n = system.solverparams.nstart;
 
@@ -21,15 +40,20 @@ end
 
 tic();
 while system.solverparams.numbeats < system.solverparams.numbeatstotal
+    tic();
     CVModule.predictorfluxes!(system,n);
     CVModule.predictorstep!(system,n);
     CVModule.correctorfluxes!(system,n);
     CVModule.correctorstep!(system,n);
+    times.tfd += toq();
+    tic();
     if hemoflag == "no"
         CVModule.applyendbcs!(system,n);
     elseif hemoflag == "yes"
         CVModule.applyendbcs!(system,n,hemoflag);
     end
+    times.tc += toq();
+    tic();
     CVModule.splitinvariants!(system,n);
     if hemoflag == "no"
         CVModule.solvesplits!(system,n);
@@ -37,14 +61,17 @@ while system.solverparams.numbeats < system.solverparams.numbeatstotal
         CVModule.solvesplits!(system,n,hemoflag);
         # CVModule.applytourniquet!(system,n); # turn off to allow continual bleeding
     end
+    times.ts += toq();
+    tic();
     CVModule.arterialpressure!(system,n);
     CVModule.regulateall!(system,n);
     if coupleflag == "yes"
         CVModule.senddata(system,n,sender);
     end
     n+=1;
+    times.tr += toq();
 end
-toc();
+times.tt += toc();
 
 CVModule.updatevolumes!(system,n);
 
@@ -54,11 +81,11 @@ if coupleflag == "yes"
 end
 
 if saveflag == "yes"
-    file = MAT.matopen("hemo3.mat", "w")
+    file = MAT.matopen("test3.mat", "w")
     write(file, "system", system)
     close(file)
 end
 
-return system, n
+return system, n, times
 
 end
